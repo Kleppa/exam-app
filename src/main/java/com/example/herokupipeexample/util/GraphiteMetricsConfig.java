583d440979ec9a5ebdc84abb5421698505e0f6dd
kleppa <Jarandwaagekleppa@gmail.com>
2018-11-14 17:21:27 +0100

    yellow2 snow

--- src/main/java/com/example/herokupipeexample/util/GraphiteMetricsConfig.java
+++ src/main/java/com/example/herokupipeexample/util/GraphiteMetricsConfig.java
@@ -19,7 +19,7 @@ public class GraphiteMetricsConfig {
 
     @Bean
     public GraphiteReporter getReporter(MetricRegistry registry) {
-        Graphite graphite = new Graphite(new InetSocketAddress(System.getenv("GRAPHITE_HOST"), 2003));
+        Graphite graphite = new Graphite(new InetSocketAddress(System.getenv("GRAPHIT_HOST"), 2003));
         GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
                 .prefixedWith(System.getenv("HOSTEDGRAPHITE_APIKEY"))
                 .convertRatesTo(TimeUnit.SECONDS)
