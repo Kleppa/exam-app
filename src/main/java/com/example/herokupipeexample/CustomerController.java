f7b24288914d835496afebb1fc442ee5ae860f94
kleppa <Jarandwaagekleppa@gmail.com>
2018-11-14 16:52:44 +0100

    test

--- src/main/java/com/example/herokupipeexample/CustomerController.java
+++ src/main/java/com/example/herokupipeexample/CustomerController.java
@@ -2,6 +2,9 @@ package com.example.herokupipeexample;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+
+import com.codahale.metrics.MetricRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +16,16 @@ public class CustomerController {
 
     private CustomerRepository customerRepository;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    @Autowired
+    public CustomerController(CustomerRepository customerRepository ,MetricRegistry registry) {
       this.customerRepository = customerRepository;
+        this.registry = registry;
     }
-
+    private final MetricRegistry registry;
     @RequestMapping("/")
     public String welcome() {
+        registry.meter("welcome").mark();
+
         return "Welcome to this small REST service. It will accept a GET on /list with a request parameter lastName, and a POST to / with a JSON payload with firstName and lastName as values.";
     }
 
