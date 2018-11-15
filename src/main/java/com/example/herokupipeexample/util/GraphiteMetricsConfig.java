eb237e028f59fabb7623e59be82422116afe0bf1
kleppa <Jarandwaagekleppa@gmail.com>
2018-11-15 17:00:18 +0100

    lol2

--- src/main/java/com/example/herokupipeexample/util/GraphiteMetricsConfig.java
+++ src/main/java/com/example/herokupipeexample/util/GraphiteMetricsConfig.java
@@ -17,7 +17,7 @@ public class GraphiteMetricsConfig {
     private String host;
     @Value("${graphite.apiKey}")
     private String apikey;
-    @Bean//ding dong
+    @Bean//ding dong dong ding
     public MetricRegistry getRegistry() {
         return new MetricRegistry();
     }
